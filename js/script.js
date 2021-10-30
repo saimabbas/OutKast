c7b379f44aac2430d6fda515fbf8de74c43c8dba
Saim Abbas <saimabbas220022@gmail.com>
2021-10-30 20:33:50 +0500

    Did mobile responsiveness of mission pages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-/* $('.js-tilt').tilt({
-    glare: true,
-    maxGlare: .5
-}) */
+$(".open-mob-header").click(() => {
+  $(".mob-header").css({
+    display: "block",
+  });
+});
+$(".close-mob-header").click(() => {
+  $(".mob-header").css({
+    display: "none",
+  });
+});
+var swiper = new Swiper(".character-swiperss", {
+  slidesPerView: 1,
+  spaceBetween: 15,
+  freeMode: true,
+});
+$(".show-characters").click(() => {
+  $(".mission-grid-dropdown").slideToggle(0);
+  $(".character-container main i").toggleClass("rotate-icon");
+});
