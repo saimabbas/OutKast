897e380d0bd4cea67ef72ec03c6620dd76be505a
Saim Abbas <saimabbas220022@gmail.com>
2021-11-07 14:16:27 +0500

    Did home page

--- js/script.js
+++ js/script.js
@@ -13,6 +13,35 @@ var swiper = new Swiper(".character-swiperss", {
   spaceBetween: 15,
   freeMode: true,
 });
+var swiper = new Swiper(".showcase-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 25,
+  loop: true,
+  navigation: {
+    nextEl: ".showcase-right",
+    prevEl: ".showcase-left",
+  },
+  breakpoints: {
+    850: {
+      slidesPerView: 4,
+    },
+  },
+});
+var swiper = new Swiper(".team-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 25,
+  loop: true,
+  navigation: {
+    nextEl: ".team-right",
+    prevEl: ".team-left",
+  },
+
+  breakpoints: {
+    850: {
+      slidesPerView: 4,
+    },
+  },
+});
 $(".show-characters").click(() => {
   $(".mission-grid-dropdown").slideToggle(0);
   $(".character-container main i").toggleClass("rotate-icon");
